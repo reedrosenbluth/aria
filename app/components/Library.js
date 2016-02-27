cbe168200c03021938d1defa0817ddca85799130
Reed Rosenbluth <Reed.Rosenbluth@gmail.com>
2016-02-27 16:36:51 -0500

    react boilerplate

---
+++ app/components/Library.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+import styles from './Library.css';
+
+class Library extends Component {
+  static propTypes = {
+    playAlbum: PropTypes.func.isRequired,
+    albums: PropTypes.func.isRequired
+  };
+  
+  render() {
+    const { playAlbum } = this.props;
+    return (
+      <div className={'container-fluid'}>
+        <div className={`${styles.albumGrid} col-lg-9`}>
+          {albums.map(function(album, i) {
+            return <Album album={album} key={i}/> 
+          })}
+        </div>
+      </div>
+    );
+  }
+}
